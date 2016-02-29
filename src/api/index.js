49f40222d21eff5eda09ec94202b86f29e0e9918
FeminismIsAwesome <ian.norris.1991@gmail.com>
2016-02-28 21:31:43 -0800

    first pass at linkedin auth crap

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,30 @@
+var Linkedin = require('node-linkedin')( '753q6tuln2wr0s', 'lK27ABwIJOYFe8Uz');//TODO put id/secret in config file
+Linkedin.auth.setCallback('/oauth/linkedin/callback');
+var scope = ['r_basicprofile',  'r_emailaddress'];
+
 function sayHello(request, reply) {
   'use strict';
 
   reply({
     hello: request.params.name
   });
+}
+
+function requestAuth(request, reply) {
+  'use strict';
+   Linkedin.auth.authorize(reply, scope);
+}
+
+function linkedInOAUTH(request, reply) {
+   Linkedin.auth.getAccessToken(res, req.query.code, req.query.state, function(err, results) {
+        if ( err )
+            return console.error(err);
+
+        console.log("I AUTHENTICATED WITH LINKEDIN!")
+
+        console.log(results);
+        return res.redirect('/');
+    });
 }
 
 function register(server, options, next) {
@@ -13,6 +34,17 @@ function register(server, options, next) {
     method: 'GET',
     path: '/hello/{name}',
     handler: sayHello
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/oauth/linkedin/callback',
+    handler: linkedInOAUTH
+  });
+  server.route({
+    method: 'GET',
+    path: '/oauth/linkedin',
+    handler: requestAuth
   });
 
   return next();
