dfc0d636ff9f6ac1efe9bdbef1fe4f7bd27f263c
FeminismIsAwesome <ian.norris.1991@gmail.com>
2016-03-05 17:59:16 -0800

    endless amusement

--- models/index.js
+++ models/index.js
@@ -11,10 +11,15 @@ var db        = {};
 console.log("LOADED?")
 if (config.use_env_variable) {
   console.log("YE")
-  var sequelize = new Sequelize(process.env.DATABASE_URL, {
+  var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+
+    sequelize = new Sequelize(match[5], match[1], match[2], {
       dialect:  'postgres',
-      protocol: 'postgres'
-    });
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+  })
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
