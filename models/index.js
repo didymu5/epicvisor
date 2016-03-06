affe4e59ae074235263791e01cbe967b4bfeb7c9
FeminismIsAwesome <ian.norris.1991@gmail.com>
2016-03-05 17:57:55 -0800

    pretty sure the instructions are borked

--- models/index.js
+++ models/index.js
@@ -13,10 +13,7 @@ if (config.use_env_variable) {
   console.log("YE")
   var sequelize = new Sequelize(process.env.DATABASE_URL, {
       dialect:  'postgres',
-      protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
-      logging:  true //false
+      protocol: 'postgres'
     });
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
