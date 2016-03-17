dde969d41bbdf56155ab69cabf3024aee31204d9
tdubs <thomas.wu@kp.org>
2016-03-17 00:41:58 -0700

    adding webpack config

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: "./app/app.js",
+  output: {
+    filename: "public/js/bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: [/\.es6$/, /\.js$/],
+      exclude: ['/node_modules/', '/src/'],
+      loader: 'babel-loader',
+      query: {
+        cacheDirectory: true,
+        presets: ['es2015']
+      }
+    }]
+  },
+  resolve: {
+    modulesDirectories: ['node_modules'],   
+    extensions: ['', '.js', '.es6']
+  }
+}
