0ef5e995ffc804d073aa02ee9a240c03c171bb65
tdubs <thomas.wu@kp.org>
2016-03-17 00:40:43 -0700

    moved angular app stuff to /app directory

---
+++ app/app.js
@@ -0,0 +1,32 @@
+
+'use strict';
+import angular from 'angular/angular';
+import angular_routes from 'angular-route/angular-route';
+
+// Declare app level module which depends on filters, and services
+
+var myApp = angular.module('myApp', [
+  'ngRoute'
+]).
+config(function ($routeProvider, $locationProvider) {
+  $routeProvider.
+    when('/home', {
+      templateUrl: 'templates/home.html',
+      controller: 'HomeController'
+    }).
+    otherwise({
+      redirectTo: '/home'
+    });
+
+  // $locationProvider.html5Mode(true);
+});
+
+myApp.controller('ApplicationController', function($scope) {
+  $scope.hello = "Hello linkedin!"
+}).controller('HomeController', function($scope, $http) {
+  $http.get('/user/info').then(function(res) {
+    if(res.data) {
+      $scope.user = res.data;
+    }
+  })
+});
