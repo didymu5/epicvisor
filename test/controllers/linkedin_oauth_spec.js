90827d2d766aa5999810390a7d619b974a2e027e
FeminismIsAwesome <ian.norris.1991@gmail.com>
2016-03-26 10:51:55 -0700

    first pass at adding a test for linkedin oauth

--- test/controllers/linkedin_oauth_spec.js
+++ test/controllers/linkedin_oauth_spec.js
@@ -31,6 +31,14 @@ var mockLinkedIn = function(apiKey, apiSecret) {
 describe('Redirect to authorization', function() {
   'use strict';
 
+  before(function(done) {
+    var db_setup = require('../setup/db_setup');
+    return db_setup.prepare().then(function() {
+      console.log("SU!");
+      done();
+    });
+  });
+
   it('tries to authenticate with scope', function(done) {
     var replyThingie = {};
     var reply, scope;
@@ -45,4 +53,65 @@ describe('Redirect to authorization', function() {
     expect(linkedInSpy.getCallback()).to.equal("testurl/oauth/linkedin/callback");
     done();
   });
+  it("does oAuth", function(done) {
+
+    var linkedInClient = {
+      people: {
+        me: function(stuffs, linkedInFn) {
+          expect(stuffs).to.deep.equal( [
+        "id",
+        "first-name",
+        "last-name",
+        "picture-url",
+        "headline",
+        "location",
+        "industry",
+        "summary",
+        "positions",
+        "specialties",
+        "public-profile-url",
+        "email-address"
+      ]);
+          linkedInFn(undefined, {
+            firstName: 'Ian',
+            lastName: 'Norris',
+            id: 'boop'
+          });
+        }
+      } 
+    }
+    var replyThingie = {redirect: function(url) {
+      expect(url).to.equal("/index.html#/profile");
+      User.findOne().then(function(user) {
+        expect(user.first_name).to.equal('Ian');
+        expect(user.last_name).to.equal('Norris');
+        expect(user.linkedin_id).to.equal('boop');
+        done();
+      });
+    }};
+    var request = {
+      yar: {
+        set: function(attrName, attrValue) {
+          expect(attrName).to.equal("user")
+          expect(attrValue.id).to.equal(1);
+        }
+      },
+      query: {
+        code: 'code',
+        state: 'state'
+      }
+    }
+    var reply, scope;
+    linkedInSpy.auth.getAccessToken = function(reply, code, state, callback) {
+      expect(code).to.equal('code');
+      expect(state).to.equal('state');
+      callback(undefined, {access_token: 'superSecretToken'});
+    };
+    linkedInSpy.init = function(access_token) {
+      expect(access_token).to.equal('superSecretToken');
+      return linkedInClient;
+    }
+    var linkedInController = require('../../src/api/linked_in_controller').initialize(mockLinkedIn);
+    linkedInController.linkedInOAUTH(request, replyThingie);
+  })
 });
