6e0ddc6cb67ec84aa1ac056d41fcad28e699ebfd
tdubs <thomas.wu@kp.org>
2016-03-31 19:07:24 -0700

    mergeing sessions.js

--- models/sessions.js
+++ models/sessions.js
@@ -1,31 +1,31 @@
 var db = require('./index');
 var Sequelize = db.Sequelize;
-var Session = db.sequelize.define('sessions',{
+var Session = db.sequelize.define('sessions', {
   id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      user_id: {
-        type: Sequelize.STRING
-      },
-      date: {
-        type: Sequelize.DATE
-      },
-      status: {
-        type: Sequelize.STRING
-      },
-      student_id: {
-        type: Sequelize.INTEGER
-      },
-      day: {
-        type: Sequelize.DATE
-      },
-      startTime: {
-        type: Sequelize.DATE
-      },
-      endTime: {
-        type: Sequelize.DATE
-      }
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  user_id: {
+    type: Sequelize.STRING
+  },
+  date: {
+    type: Sequelize.DATE
+  },
+  status: {
+    type: Sequelize.STRING
+  },
+  student_id: {
+    type: Sequelize.INTEGER
+  },
+  day: {
+    type: Sequelize.DATE
+  },
+  startTime: {
+    type: Sequelize.DATE
+  },
+  endTime: {
+    type: Sequelize.DATE
+  }
 })
 module.exports = Session;
