495ac5a2b28ac34fcd30d162136d5f9c13d444a5
FeminismIsAwesome <ian.norris.1991@gmail.com>
2016-04-03 19:29:00 -0700

    adding searching

--- app/controllers/profile_search_controller.js
+++ app/controllers/profile_search_controller.js
@@ -1,16 +1,25 @@
 import _ from 'underscore';
 function profileSearchController($scope, user, mentors, $location, mentorService) {
   $scope.user = user;
-  var industries = _.uniq(mentors.map(function(mentor){ return mentor.industry}));
+  var allMentors = mentors;
+    function getMentorCompany(mentor) {
+    return mentor.positions && mentor.positions.values && mentor.positions.values[0].company.name;
+  }
+  function getIndustryCompany(mentor) {
+    return mentor.industry;
+  }
+  function getCareerTopics(mentor) {
+    return mentor.career_topics;
+  }
+  var industries = _.uniq(mentors.map(getIndustryCompany));
   $scope.industries = industries.map(function(industry) {
     return {
       ticked: false,
       name: industry
     }
   });
-  var companies = _.uniq(mentors.map(function(mentor) {
-    return mentor.positions && mentor.positions.values && mentor.positions.values[0].company.name
-  }))
+
+  var companies = _.uniq(mentors.map(getMentorCompany));
   companies = _.filter(companies, function(company) { return company !== undefined});
   $scope.companies = companies.map(function(company) {
     return {
@@ -19,9 +28,7 @@ function profileSearchController($scope, user, mentors, $location, mentorService
     }
   });
 
-  var fields = _.uniq(_.flatten(mentors.map(function(mentor) {
-    return mentor.career_topics || [];
-  })));
+  var fields = _.uniq(_.flatten(mentors.map(getCareerTopics)));
   $scope.fields = fields.map(function(field) {
     return {
       ticked: false,
@@ -31,9 +38,23 @@ function profileSearchController($scope, user, mentors, $location, mentorService
 
   $scope.mentors = mentors;
   $scope.search = function() {
-    mentorService.getMentors().then(function(mentors) {
-      $scope.mentors = mentors;
-    })
+    var filteredMentors = allMentors;
+    if($scope.selectedIndustries.length >0 ) {
+      filteredMentors = filteredMentors.filter(function(mentor) {
+        return _.map($scope.selectedIndustries, 'name').indexOf(mentor.industry) !== -1;
+      });
+    }
+    if($scope.selectedField.length > 0) {
+      filteredMentors = filteredMentors.filter(function(mentor) {
+        return _.map($scope.selectedField, 'name').indexOf(getCareerTopics(mentor)) !== -1;
+      });
+    }
+    if($scope.selectedCompanies.length > 0) {
+      filteredMentors = filteredMentors.filter(function(mentor) {
+        return _.map($scope.selectedCompanies, 'name').indexOf(getMentorCompany(mentor)) !== -1;
+      });
+    }
+    $scope.mentors = filteredMentors;
   }
   $scope.routeToMentor = function(mentor) {
     $location.path('/mentors/' + mentor.user_id);
