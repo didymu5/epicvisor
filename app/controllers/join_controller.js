7351f3098d41c719e6640b75c775d464173dd789
tdubs <thomas.wu@kp.org>
2016-04-03 22:41:31 -0700

    making join more complicated

--- app/controllers/join_controller.js
+++ app/controllers/join_controller.js
@@ -1,9 +1,11 @@
 function joinController($scope, $location) {
-
-  if ($location.search()['epemba']) {
+  
+  if ($location.search()['9aa228a3f6d2958438eae5d3d40fb348']) {
     console.log('hi');
     $scope.linkedInButton = "button"
-  };
+  } else {
+    $location.url('')
+  }
 }
 joinController.$inject = ['$scope', '$location'];
 export default joinController;
