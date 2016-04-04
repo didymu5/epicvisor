a2a7c9e2fc48a8352c203e490be7b2013e9dc76c
tdubs <thomas.wu@kp.org>
2016-04-03 21:28:06 -0700

    adding styles and control for join

--- app/controllers/join_controller.js
+++ app/controllers/join_controller.js
@@ -1,5 +1,9 @@
-function joinController($scope) {
-  $scope.inviteCode = "epemba";
+function joinController($scope, $location) {
+
+  if ($location.search()['epemba']) {
+    console.log('hi');
+    $scope.linkedInButton = "button"
+  };
 }
-joinController.$inject = ['$scope'];
+joinController.$inject = ['$scope', '$location'];
 export default joinController;
