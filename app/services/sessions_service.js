220ebf75a970e39740117b1de5f8d35f6fb9fdb4
tdubs <thomas.wu@kp.org>
2016-07-17 15:04:07 -0700

    formating code

--- app/services/sessions_service.js
+++ app/services/sessions_service.js
@@ -1,9 +1,10 @@
 import moment from 'moment';
 import _ from 'underscore';
+
 function sessionsService($http, userService, mentorService, $q, studentService) {
-	var sessionState = {};
-   
-	function makeSessions(sessions, sessionState) {
+    var sessionState = {};
+
+    function makeSessions(sessions, sessionState) {
         var numberOfSessionsToGenerate = sessionState.sessionCount;
         var sessionCountType = sessionState.sessionCountType;
         var twoDaysAgo = moment().startOf('day').subtract(1, 'days');
@@ -16,15 +17,15 @@ function sessionsService($http, userService, mentorService, $q, studentService) 
         var orderedSessions = [];
         var weeks = _.keys(sessionsByWeek);
         var increment = 1;
-        if(sessionCountType === "Every Other Week") {
-            increment = 2;    
+        if (sessionCountType === "Every Other Week") {
+            increment = 2;
         }
-        for(var i=0; i< 4; i+=increment) {
+        for (var i = 0; i < 4; i += increment) {
             var currentWeek = moment().add('weeks', i).startOf('day');
             var match = _.find(weeks, function(week) {
-                return moment(week).isSame(currentWeek,'week');
+                return moment(week).isSame(currentWeek, 'week');
             });
-            if(!match) {
+            if (!match) {
                 sessionsByWeek[currentWeek.startOf('week').toString()] = [];
                 weeks.push(currentWeek.startOf('week').toString());
             }
@@ -34,110 +35,110 @@ function sessionsService($http, userService, mentorService, $q, studentService) 
         }).forEach(function(week) {
             var sessionsForWeek = sessionsByWeek[week];
 
-            for(var sessionNumber =sessionsForWeek.length; sessionNumber < numberOfSessionsToGenerate; sessionNumber++ )
-            {
-              sessionsForWeek.push({
+            for (var sessionNumber = sessionsForWeek.length; sessionNumber < numberOfSessionsToGenerate; sessionNumber++) {
+                sessionsForWeek.push({
                     date: moment(week).toDate(),
-                  number: sessionNumber+1,
-                  status: 'Open'
+                    number: sessionNumber + 1,
+                    status: 'Open'
                 })
             }
             orderedSessions = orderedSessions.concat(sessionsForWeek);
         });
-	    return orderedSessions.map(function(session) {
+        return orderedSessions.map(function(session) {
             session.date = moment(session.date).startOf('week').toDate();
             return session;
         });
-	  }
-	return {
+    }
+    return {
         makeSessions: makeSessions,
-		 getSessions: function() {
-      var self = this;
-      return $http.get('/sessions/user').then(function(res) {
-        var sessions = res.data;
-        return userService.getSessionSettings().then(function(sessionState) {       
-          return makeSessions(sessions, sessionState);
-        })
-      });
-    },
-    getSession: function(sessionid) {
-        return $http.get('/sessions/' + sessionid).then(function(res) {
-            var session = res.data;
-            if(!session) {
-                throw new Error("test");
-            }
-            var mentorId = session.user_id;
-            var studentId = session.student_id;
-            return $q.all([mentorService.getMentor(mentorId), studentService.getStudent(studentId)])
-            .then(function(results) {
-                session.mentor = results[0];
-                session.student = results[1];
-                return session;
+        getSessions: function() {
+            var self = this;
+            return $http.get('/sessions/user').then(function(res) {
+                var sessions = res.data;
+                return userService.getSessionSettings().then(function(sessionState) {
+                    return makeSessions(sessions, sessionState);
+                })
             });
-        });
+        },
+        getSession: function(sessionid) {
+            return $http.get('/sessions/' + sessionid).then(function(res) {
+                var session = res.data;
+                if (!session) {
+                    throw new Error("test");
+                }
+                var mentorId = session.user_id;
+                var studentId = session.student_id;
+                return $q.all([mentorService.getMentor(mentorId), studentService.getStudent(studentId)])
+                    .then(function(results) {
+                        session.mentor = results[0];
+                        session.student = results[1];
+                        return session;
+                    });
+            });
 
-    },
-    confirmTime: function(session, day, startTime, endTime) {
-        return $http.post('/sessions/' + session.id + '/update', 
-        {
-            data: {
-                day: day,
-                startTime: startTime,
-                endTime: endTime
-            }
-        }).then(function(res) {
-            return res.data;
-        });
-    },
-    cancelTime: function(session) {
-        return $http.post('/sessions/' + session.id + '/destroy').then(function(res){
-            return res.data;
-        });
-    },
-    storeMentor: function(mentor) {
-    	sessionState.mentor = mentor;
-    },
-    storeSession: function(session) {
-    	sessionState.session = session;
-    },
-    getCurrentSession: function() {
-    	return sessionState.session;
-    },
-    getSessionMentor: function(){
-    	return sessionState.mentor;
-    },
-    confirmSession: function(session, mentor, student) {
-    	return $http.post('/student/verify', {data: student}).then(function(res) {
-            var studentData = res.data;
-    		if(studentData) {
-                session.student_id = studentData.id;
-                session.status = 'pending';
-    			return $http.post('/mentor/' + mentor.user_id +  '/sessions/appointment', 
-                    session).then(function(session) {
+        },
+        confirmTime: function(session, day, startTime, endTime) {
+            return $http.post('/sessions/' + session.id + '/update', {
+                data: {
+                    day: day,
+                    startTime: startTime,
+                    endTime: endTime
+                }
+            }).then(function(res) {
+                return res.data;
+            });
+        },
+        cancelTime: function(session) {
+            return $http.post('/sessions/' + session.id + '/destroy').then(function(res) {
+                return res.data;
+            });
+        },
+        storeMentor: function(mentor) {
+            sessionState.mentor = mentor;
+        },
+        storeSession: function(session) {
+            sessionState.session = session;
+        },
+        getCurrentSession: function() {
+            return sessionState.session;
+        },
+        getSessionMentor: function() {
+            return sessionState.mentor;
+        },
+        confirmSession: function(session, mentor, student) {
+            return $http.post('/student/verify', {
+                data: student
+            }).then(function(res) {
+                var studentData = res.data;
+                if (studentData) {
+                    session.student_id = studentData.id;
+                    session.status = 'pending';
+                    return $http.post('/mentor/' + mentor.user_id + '/sessions/appointment',
+                        session).then(function(session) {
                         return true;
                     })
-		}
-    		else {
-    			return false;
-    		}
-    	})
-    },
-    getMentorsAndSessions: function() {
-        return $http.get('/mentors/sessions').then(function(res) {
-            return res.data;
-        });
-    },
-    getMentorSessions: function(mentorId) {
-		var self = this;
-		  return $http.get('/mentor/' + mentorId + '/sessions').then(function(res) {
-		    var sessions = res.data;
-		    return mentorService.getMentorSettings(mentorId).then(function(sessionState) {       
-		      return makeSessions(sessions, sessionState);
-		    })
-		  });
-    }
-	};
+                } else {
+                    return false;
+                }
+            })
+        },
+        getMentorsAndSessions: function() {
+            return $http.get('/mentors/sessions').then(function(res) {
+                console.log(res.data);
+                return res.data;
+            });
+        },
+        getMentorSessions: function(mentorId) {
+            var self = this;
+            return $http.get('/mentor/' + mentorId + '/sessions').then(function(res) {
+                var sessions = res.data;
+                return mentorService.getMentorSettings(mentorId).then(function(sessionState) {
+                    return makeSessions(sessions, sessionState);
+                })
+            });
+        }
+    };
 }
 
-sessionsService.$inject =['$http', 'userService','mentorService', '$q', 'studentService'];
+sessionsService.$inject = ['$http', 'userService', 'mentorService', '$q', 'studentService'];
 export default sessionsService;
