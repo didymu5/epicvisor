0f88f0edd84e22cda63c0ddda79910677e45418b
tdubs <thomas.wu@kp.org>
2016-08-06 14:17:08 -0700

    formatting code cause im anal

--- models/user_profile.js
+++ models/user_profile.js
@@ -1,19 +1,19 @@
 var db = require('./index');
 var Sequelize = db.Sequelize;
-var UserProfile = db.sequelize.define('user_profiles',{
+var UserProfile = db.sequelize.define('user_profiles', {
   id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      year: {
-        type: Sequelize.STRING
-      },
-      blurb: {
-        type: Sequelize.TEXT
-      },
-      user_id: {
-        type: Sequelize.INTEGER
-      }
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  year: {
+    type: Sequelize.STRING
+  },
+  blurb: {
+    type: Sequelize.TEXT
+  },
+  user_id: {
+    type: Sequelize.INTEGER
+  }
 })
 module.exports = UserProfile;
