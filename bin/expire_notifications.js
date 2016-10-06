8148b42290fe703f297f0050dd6d7bb2c6c07e41
tdubs <thomas.wu@kp.org>
2016-10-05 22:14:05 -0700

    formating js

--- bin/expire_notifications.js
+++ bin/expire_notifications.js
@@ -1,14 +1,13 @@
 var sequelize = require('..//models').sequelize;
 var Session = require('../models/sessions');
 var moment = require('moment');
-return Session.update(
-	{ status: 'expired'},
-     {
-     	where: {
-     		status: 'pending',
-            createdAt: {
-            	$lte: moment().add(-2, 'days').toDate()
-            }
-     	}
-     }
-	)
+return Session.update({
+    status: 'expired'
+}, {
+    where: {
+        status: 'pending',
+        createdAt: {
+            $lte: moment().add(-2, 'days').toDate()
+        }
+    }
+})
